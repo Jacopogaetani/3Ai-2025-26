bf1c445d88b6b1496c47f6ed42cd9909eb0a55ac
Jacopo Gaetani <160734900+Jacopogaetani@users.noreply.github.com>
2025-09-21 16:45:03 +0200

    Update sommaNumeri.cpp

--- Informatica/C++/Esercitazione 3/sommaNumeri.cpp
+++ Informatica/C++/Esercitazione 3/sommaNumeri.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
-
+/*Calcolare la somma dei primi N numeri naturali.
+	ANALISI DEL PROBLEMA:
+	Dati in input: N, intero di nome somma inizializzato a 0
+	Dati in output: somma dei primi N numeri naturali
+	Strategia risolutiva: Dopo aver ricevuto in input il numero n, si crea un ciclo for (ciclo definito) dove ad ogni iterazione "i" si va a sommare al valore di "somma"
+	Ottenendo così la somma dei primi N numeri interi*/
 int main()
 {
 	int n, somma = 0;
@@ -19,3 +24,4 @@ int main()
 	
 	return 0;
 }
+
