e7b6d697ad840b252c77a2435ffc3ef31663c25e
Jacopo Gaetani <160734900+Jacopogaetani@users.noreply.github.com>
2025-09-21 16:44:05 +0200

    Update numeriPariMinoriDiN_Gaetani3Ai.cpp

--- Informatica/C++/Esercitazione 3/numeriPariMinoriDiN_Gaetani3Ai.cpp
+++ Informatica/C++/Esercitazione 3/numeriPariMinoriDiN_Gaetani3Ai.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-	/*Stampare i numeri pari minori di N.*/
+	/*Stampare i numeri pari minori di N.
+ 	ANALISI DEL PROBLEMA
+	Dati in input: numero n
+	Dati in output: numeri pari minori di n
+	Strategia risolutiva: Con un ciclo for (ciclo definito) che parte con la variabile i con il valore di n a essere decrementata di 1 (in modo tale che si possano analizzare i
+	numeri minori di n). Con un costrutto if si verifica il resto della divisione di per 2, se il resto è 0 (quindi il numero è pari) viene stampato a schermo i, altrimenti il
+	programma passa avanti*/
+	
 	int n;
 	
 	cout<<"Inserisci un numero: ";
@@ -20,3 +27,4 @@ int main()
 	
 	return 0;
 }
+
