c1b0bea9a4219dd406c52364d7139e0e2e5d560e
JacopoHasFunProgramming <160734900+Jacopogaetani@users.noreply.github.com>
2026-02-17 11:46:58 +0100

    Delete Informatica/Java/Esercitazione1 directory

--- Informatica/Java/Esercitazione1/Esercizio2_Gaetani3Ai.java
+++
@@ -1,9 +0,0 @@
-public class Esercizio2_Gaetani3Ai {
-    public static void main(String[] args){
-        int base = 5, altezza = 3, area;
-
-        area = base * altezza;
-
-        System.out.println("Base " +  base + "\nAltezza: " + altezza + "\nArea: " + area);
-    }
-}
