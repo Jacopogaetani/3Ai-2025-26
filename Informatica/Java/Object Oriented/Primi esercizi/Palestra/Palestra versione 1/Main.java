c93e17600ad3f2dea1b422442244febe492ae181
JacopoHasFunCoding <160734900+Jacopogaetani@users.noreply.github.com>
2026-03-14 11:27:43 +0100

    Add files via upload

---
+++ Informatica/Java/Object Oriented/Primi esercizi/Palestra/Palestra versione 1/Main.java
@@ -0,0 +1,37 @@
+package Palestra;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Palestra p1 = new Palestra("Virgin Active", 100, 100, true, 90);
+        Palestra p2 = new Palestra("Pacco palestrina", 160, 90, true, 85);
+
+        p1.entraPersona();
+        p1.escePersona();
+        p1.chiudiPalestra();
+        p1.apriPalestra();
+        p1.aggiungiIscritto(5);
+        System.out.println(p1.palestraPiena());
+        p1.confrontaIscritti(p2);
+        p1.confrontaCapienza(p2);
+        System.out.println(p1.infoPalestra());
+
+        System.out.println("\n ");
+
+        p2.entraPersona();
+        p2.escePersona();
+        p2.chiudiPalestra();
+        p2.apriPalestra();
+        p2.aggiungiIscritto(10);
+        System.out.println(p2.palestraPiena());
+        p2.confrontaIscritti(p1);
+        p2.confrontaCapienza(p1);
+        System.out.println(p2.infoPalestra());
+
+
+
+
+
+
+    }
+}
