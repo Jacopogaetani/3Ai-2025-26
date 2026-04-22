ee0399924cd92f83fdac54d4f7b1285cab43f1fe
JacopoHasFunCoding <160734900+Jacopogaetani@users.noreply.github.com>
2026-04-22 08:44:13 +0200

    Update Addizione_SottrazioneGaetani.asm

--- Sistemi e reti/Assembly/Addizione_SottrazioneGaetani.asm
+++ Sistemi e reti/Assembly/Addizione_SottrazioneGaetani.asm
@@ -2,9 +2,9 @@
 
 data segment
     ; add your data here!  
-    num1 db 00h ;inizializzazione delle variabili
-    num2 db 00h
-    ris db 00h
+    num1 db 81h ;inizializzazione delle variabili
+    num2 db 81h
+    ris db ? ;non inizializzato
     
 ends
 
@@ -19,8 +19,8 @@ start:
     mov ds, ax
     mov es, ax
     
-    mov al, 81h
-    mov bl, 80h
+    mov al, num1
+    mov bl, num2
                  
     cmp al, bl ;compara i due registri attraverso una sottrazione
     jne sott; se non solo uguali va al ramo "sott" controllando lo zf (se al - bl = 0 allora sono uguali)
